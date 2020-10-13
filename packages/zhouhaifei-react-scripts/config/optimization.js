9ff32560dc0105ca7dd7049676ffaed7bedad6ad
joey <joey@egenie.cn>
2020-10-13 21:23:05 +0800

    feat(*): update package

--- packages/zhouhaifei-react-scripts/config/optimization.js
+++ packages/zhouhaifei-react-scripts/config/optimization.js
@@ -65,8 +65,8 @@ module.exports = {
      https://twitter.com/wSokra/status/969633336732905474
      https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366 */
   splitChunks: {
-    /* chunks: 'all',
-       minSize: utils.splitChunkMinSize, */
+    chunks: 'all',
+    minSize: utils.splitChunkMinSize,
     name: false,
     cacheGroups: {},
   },
