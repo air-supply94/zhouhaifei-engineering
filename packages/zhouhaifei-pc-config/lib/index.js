631722ce036b9b2eb30894c52b2512fad5c91d24
joey <joey@egenie.cn>
2023-02-02 11:18:38 +0800

    feat(*): 去掉mobx、redux等的externals

--- packages/zhouhaifei-pc-config/lib/index.js
+++ packages/zhouhaifei-pc-config/lib/index.js
@@ -1,11 +1,7 @@
 const babel = require('./babel');
 const postcss = require('./postcss');
-const webpackConfig = require('./webpackConfig');
-const theme = require('./theme');
 
 module.exports = {
   babel,
   postcss,
-  webpackConfig,
-  theme
 };
--- packages/zhouhaifei-pc-config/lib/theme/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(extraTheme = {}) {
-  return { ...extraTheme, };
-};
-
-
-
