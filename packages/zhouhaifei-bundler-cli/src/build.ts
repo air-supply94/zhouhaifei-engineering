319bfbb3efc1a266d0079a7517876af0dbba76b1
zhouhaifei <zhouhaifei1994@sina.com>
2024-05-06 23:02:40 +0800

    chore(*): update config

--- packages/zhouhaifei-bundler-cli/src/build.ts
+++ packages/zhouhaifei-bundler-cli/src/build.ts
@@ -2,7 +2,6 @@ import webpack from 'webpack';
 import { config } from './config/config';
 import type { BuildOptions } from './types';
 import { Env } from './types';
-import chalk from 'chalk';
 
 export async function build({
   userConfig = {},
@@ -23,8 +22,9 @@ export async function build({
       const validErr = err || (stats?.hasErrors() ? new Error(stats!.toString('errors-only')) : null);
 
       if (validErr) {
-        // try to catch esbuild minify error to output  friendly error message
-        stats?.hasErrors() && esbuildCompressErrorHelper(validErr.toString());
+        if (stats?.hasErrors()) {
+          console.error(validErr.toString());
+        }
         reject(validErr);
       } else {
         resolve(stats!);
@@ -52,39 +52,3 @@ export async function build({
   });
 }
 
-function esbuildCompressErrorHelper(errorMsg: string) {
-  if (typeof errorMsg !== 'string') {
-    return;
-  }
-
-  if (
-    // https://github.com/evanw/esbuild/blob/a5f781ecd5edeb3fb6ae8d1045507ab850462614/internal/js_parser/js_parser_lower.go#L18
-    errorMsg.includes('configured target environment') &&
-    errorMsg.includes('es2015')
-  ) {
-    const terserRecommend = {
-      label: chalk.green('change jsMinifier'),
-      details: chalk.cyan('  jsMinifier: \'terser\''),
-    };
-    const upgradeTargetRecommend = {
-      label: chalk.green('upgrade target'),
-      details: chalk.cyan(`  jsMinifierOptions: {
-    target: ['chrome80', 'es2020']
-  }`),
-    };
-    const ieRecommend = { details: 'P.S. compatible with legacy browsers: https://umijs.org/blog/legacy-browser' };
-    console.log();
-    console.log(chalk.bgRed(' COMPRESSION ERROR '));
-    console.log(
-      chalk.yellow(
-        `esbuild minify failed, please ${terserRecommend.label} or ${upgradeTargetRecommend.label}:`
-      )
-    );
-    console.log('e.g. ');
-    console.log(terserRecommend.details);
-    console.log('   or');
-    console.log(upgradeTargetRecommend.details);
-    console.log(ieRecommend.details);
-    console.log();
-  }
-}
